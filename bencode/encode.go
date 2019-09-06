dc2a769de440028ef0384e98b01b8042bfd9cc14
Loukas Litsos <lkslts64@gmail.com>
2019-09-06 15:50:50 +0300

    struct tags omitempty and omit feature & nil values encoding.tested(kind of)

--- bencode/encode.go
+++ bencode/encode.go
@@ -9,6 +9,10 @@ import (
 	"strconv"
 )
 
+//If a struct field is empty and the user wants it to be ommited from
+//the bencoded result, then a struct field tag should be added like this:
+//`empty:"omit".If a struct field should be ommited regardless of the its
+//emptiness, then a tag should be added like: `bencode:"-"
 func Encode(v interface{}) ([]byte, error) {
 	var b bytes.Buffer
 	err := encode(reflect.ValueOf(v), &b)
@@ -23,13 +27,45 @@ func Encode(v interface{}) ([]byte, error) {
 //In struct fields, if a struct tag with key=='bencode' is
 //present, then assume the key is the value of the tag. Otherwise,
 //the struct field's name will be the dicionary's key.
+//TODO: Support bigInts? (they are needed to parse .torrent files who talk about huge pieces or files)
+//int64 should be enough for most of the torrents sizes.
 func encode(v reflect.Value, b *bytes.Buffer) error {
+	if !v.IsValid() {
+		return nil
+	}
 	t := v.Type()
 	switch t.Kind() {
+	//'dereference' pointer or look 'inside' the interface.
+	case reflect.Ptr:
+		if !v.Elem().IsValid() { //propably equivilent to v.isNil()
+			handleNilValuePtr(t, b)
+		}
+		if err := encode(v.Elem(), b); err != nil {
+			return err
+		}
+	case reflect.Interface:
+		//ignore nil interfaces?- tricky decision
+		if !v.Elem().IsValid() {
+			break
+		}
+		if err := encode(v.Elem(), b); err != nil {
+			return err
+		}
 	//i<integer encoded in base ten ASCII>e
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		s := strconv.FormatInt(v.Int(), 10)
 		b.WriteString("i" + s + "e")
+	//i<integer encoded in base ten ASCII>e
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		s := strconv.FormatUint(v.Uint(), 10)
+		b.WriteString("i" + s + "e")
+	//bool is like integer values.True corresponds to 1 and false to 0.
+	case reflect.Bool:
+		if v.Bool() {
+			b.WriteString("i1e")
+			break
+		}
+		b.WriteString("i0e")
 	//<string length encoded in base ten ASCII>:<string data>
 	case reflect.String:
 		b.WriteString(strconv.Itoa(len(v.String())) + ":" + v.String())
@@ -40,11 +76,11 @@ func encode(v reflect.Value, b *bytes.Buffer) error {
 		if t.Elem().Kind() == reflect.Uint8 {
 			s := v.Bytes()
 			b.WriteString(strconv.Itoa(len(s)) + ":" + string(s))
-			return nil
+			break
 		}
 		if v.IsNil() {
 			b.WriteString("le")
-			return nil
+			break
 		}
 		b.WriteString("l")
 		for i := 0; i < v.Len(); i++ {
@@ -62,24 +98,6 @@ func encode(v reflect.Value, b *bytes.Buffer) error {
 		if v.IsNil() {
 			b.WriteString("de")
 		}
-		//one way of doing it
-		/*
-			keys := v.MapKeys()
-			skeys := make([]string, len(keys))
-			for i := 0; i < len(keys); i++ {
-				skeys[i] = keys[i].String()
-			}
-			sort.Strings(skeys)
-			b.WriteString("d")
-			for i := 0; i < len(keys); i++ {
-				b.WriteString(strconv.Itoa(len(skeys[i])) + ":" + skeys[i])
-				if err := encode(v.MapIndex(keys[i]),v); err != nil {
-					return err
-				}
-			}
-			b.WriteString("e")
-		*/
-		//another one
 		keys := string_reflect(v.MapKeys())
 		sort.Sort(keys)
 		b.WriteString("d")
@@ -105,9 +123,14 @@ func encode(v reflect.Value, b *bytes.Buffer) error {
 		sort.Sort(sf)
 		b.WriteString("d")
 		for i := 0; i < v.NumField(); i++ {
+			fvalue := v.FieldByName(sf[i].Name)
+			//if field is a nil pointer and struct tag == omitempty , then ignore this field.
+			if sf[i].Tag.Get("bencode") == "-" || (fvalue.Type().Kind() == reflect.Ptr && !fvalue.Elem().IsValid() && sf[i].Tag.Get("empty") == "omit") {
+				continue
+			}
 			//encode string and field
 			b.WriteString(strconv.Itoa(len(sf.get(i))) + ":" + sf.get(i))
-			if err := encode(v.FieldByName(sf[i].Name), b); err != nil {
+			if err := encode(fvalue, b); err != nil {
 				return err
 			}
 		}
@@ -116,6 +139,26 @@ func encode(v reflect.Value, b *bytes.Buffer) error {
 		return errors.New("Unsupported type")
 	}
 	return nil
+}
+
+func handleNilValuePtr(t reflect.Type, b *bytes.Buffer) {
+	e := t.Elem()
+	switch e.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		b.WriteString("i0e")
+	case reflect.Slice:
+		if e.Elem().Kind() != reflect.Uint8 {
+			b.WriteString("le")
+			break
+
+		}
+		fallthrough
+	case reflect.String:
+		b.WriteString("0:")
+	case reflect.Struct:
+		b.WriteString("de")
+
+	}
 }
 
 type string_reflect []reflect.Value
--- bencode/encode_test.go
+++ bencode/encode_test.go
@@ -5,13 +5,76 @@ import (
 	"testing"
 )
 
+type random_encode_test struct {
+	value    interface{}
+	expected string
+}
+
+type random_struct struct {
+	ABC         int    `bencode:"abc"`
+	SkipThisOne string `bencode:"-"`
+	CDE         string
+}
+
+type dummy struct {
+	a, b, c int
+}
+
+var random_encode_tests = []random_encode_test{
+	{int(10), "i10e"},
+	{uint(10), "i10e"},
+	{"hello, world", "12:hello, world"},
+	{true, "i1e"},
+	{false, "i0e"},
+	{int8(-8), "i-8e"},
+	{int16(-16), "i-16e"},
+	{int32(32), "i32e"},
+	{int64(-64), "i-64e"},
+	{uint8(8), "i8e"},
+	{uint16(16), "i16e"},
+	{uint32(32), "i32e"},
+	{uint64(64), "i64e"},
+	//{random_struct{123, "nono", "hello"}, "d3:CDE5:hello3:abci123ee"},
+	{map[string]string{"a": "b", "c": "d"}, "d1:a1:b1:c1:de"},
+	{[]byte{1, 2, 3, 4}, "4:\x01\x02\x03\x04"},
+	//{[4]byte{1, 2, 3, 4}, "li1ei2ei3ei4ee"},
+	{nil, ""},
+	{[]byte{}, "0:"},
+	{[]byte(nil), "0:"},
+	{"", "0:"},
+	{[]int{}, "le"},
+	{map[string]int{}, "de"},
+	//{&dummy{1, 2, 3}, "i2ei3ei4e"},
+	{struct {
+		A *string
+	}{nil}, "d1:A0:e"},
+	{struct {
+		A *string
+	}{new(string)}, "d1:A0:e"},
+	{struct {
+		A *string `empty:"omit"`
+	}{nil}, "de"},
+	{struct {
+		A *string `empty:"omit"`
+	}{new(string)}, "d1:A0:e"},
+	{struct {
+		A *string `bencode:"-"`
+	}{new(string)}, "de"},
+	/*{struct {
+		A *string `bencode:",omitempty"`
+	}{new(string)}, "d1:A0:e"},
+	{bigIntFromString("62208002200000000000"), "i62208002200000000000e"},
+	{*bigIntFromString("62208002200000000000"), "i62208002200000000000e"},*/
+}
+
 func TestEncode(t *testing.T) {
-	got, err := Encode(struct {
-		x int
-		y int
-	}{
-		5,
-		10,
-	})
-	fmt.Println(string(got), err)
+	for _, test := range random_encode_tests {
+		fmt.Println(test.expected)
+		got, err := Encode(test.value)
+		if string(got) != test.expected {
+			t.Error(err, string(got), test.value, test.expected)
+		} else {
+			fmt.Println("success:", test.value)
+		}
+	}
 }
