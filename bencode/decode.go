b096dc650e428371ebedcf55270b92250a25fe9f
Loukas Litsos <lkslts64@gmail.com>
2019-09-09 20:00:52 +0300

    Handle nil interfaces

--- bencode/decode.go
+++ bencode/decode.go
@@ -3,6 +3,7 @@ package bencode
 import (
 	"bytes"
 	"errors"
+	"io"
 	"reflect"
 	"strconv"
 )
@@ -12,81 +13,49 @@ func Decode(data []byte, v interface{}) error {
 	if e.Type().Kind() != reflect.Ptr {
 		return errors.New("v should have a pointer type")
 	}
+	val := reflect.ValueOf(v)
+	if !val.IsValid() {
+		return errors.New("Provided pointers is nil.")
+	}
 	r := benReader{bytes.NewBuffer(data)}
-	err := decode(r, reflect.ValueOf(v))
-	return err
-}
-
-/*func decode(r benReader, v reflect.Value) error {
-	b, err := r.b.ReadByte()
+	err := decode(r, val)
 	if err != nil {
-		if err == io.EOF {
-			return nil
-		}
 		return err
 	}
-	t := v.Type()
-	switch b {
-	case 'i':
-		switch t.Kind() {
-		case reflect.Bool:
-			num, err := r.readBenBool()
-			if err != nil {
-				return err
-			}
-			v.SetBool(num)
-		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			num, err := r.readBenInt()
-			if err != nil {
-				return err
-			}
-			v.SetInt(num)
-		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			num, err := r.readBenUint()
-			if err != nil {
-				return err
-			}
-			v.SetUint(num)
-		default:
-			return errors.New("bencoded buffer has an 'i' but the provieded data structure hasn't integer(bool,int,uint)  at the correspoing offset.")
-		}
-	case b >= 48 && b <= 57:
-		err = r.b.UnreadByte()
-		if err != nil {
-			return err
-		}
-		bytes, err := r.readBenString()
-		if err != nil {
-			return err
-		}
-		switch t.Kind() {
-		case reflect.String:
-			v.SetString(string(bytes))
-		case reflect.Slice && t.Elem().Kind() == reflect.Uint8:
-			v.SetBytes(bytes)
-		default:
-			return errors.New("bencoded buffer has a string  but the provieded data structure hasn't a string or a []byte at the correspoing offset.")
-		}
-	case 'l':
-		switch t.Kind() {
-		case reflect.Slice && t.Elem().Kind() != reflect.Uint8:
-			if v.Len() > 0 {
-				return errors.New("A slice length of the data structures has not length zero.")
-			}
-		default:
-
-		}
-
-	case 'd':
-	default:
+	_, err = r.b.ReadByte()
+	if err == nil || err != io.EOF {
+		return errors.New("data structure provided was filled but bencoded buffer wasn't consumed")
 	}
-
+	return nil
 }
-*/
 
+//Parse the bencoded string based on v.
+//That means that we will expect each bencoded value
+//to have type compatible with v (and not the opposite).
 func decode(r benReader, v reflect.Value) error {
+	if !v.IsValid() {
+		panic("did not expected zero value at start of decode func.Developer's mistake!")
+	}
 	t := v.Type()
 	switch v.Kind() {
+	case reflect.Interface:
+		if !v.Elem().IsValid() {
+			handleNilInterface(r, v)
+		} else if err := decode(r, v.Elem()); err != nil {
+			return err
+		}
+	case reflect.Ptr:
+		//if pointer is nil,create a new zeroed (but not nil) value of type v.Elem()
+		//and pass this to decode. After, set this value to v.Elem()
+		if !v.Elem().IsValid() {
+			e := reflect.New(t.Elem()).Elem()
+			if err := decode(r, e); err != nil {
+				return err
+			}
+			v.Elem().Set(e)
+		} else if err := decode(r, v.Elem()); err != nil {
+			return err
+		}
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		num, err := r.readBenInt()
 		if err != nil {
@@ -118,9 +87,22 @@ func decode(r benReader, v reflect.Value) error {
 				return err
 			}
 			v.SetBytes(bytes)
+			break
 		}
-		r.readBenList()
-
+		err := r.readBenList(v)
+		if err != nil {
+			return err
+		}
+	case reflect.Map:
+		err := r.readBenDictMap(v)
+		if err != nil {
+			return err
+		}
+	case reflect.Struct:
+		err := r.readBenDictStruct(v)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
@@ -129,7 +111,6 @@ type benReader struct {
 	b *bytes.Buffer
 }
 
-//extracts the data from a bencoded string.
 func (r benReader) readBenString() ([]byte, error) {
 	lenbytes, err := r.b.ReadString(byte(':'))
 	if err != nil {
@@ -146,7 +127,6 @@ func (r benReader) readBenString() ([]byte, error) {
 	return str, nil
 }
 
-//extracts the data from a bencoded int and returns as an int
 func (r benReader) readBenInt() (int64, error) {
 	benInt, err := r.b.ReadString(byte('e'))
 	if err != nil {
@@ -163,7 +143,6 @@ func (r benReader) readBenInt() (int64, error) {
 
 }
 
-//extracts the data from a bencoded int and returns it as an uint.
 func (r benReader) readBenUint() (uint64, error) {
 	benInt, err := r.b.ReadString(byte('e'))
 	if err != nil {
@@ -175,7 +154,6 @@ func (r benReader) readBenUint() (uint64, error) {
 	return strconv.ParseUint(benInt[1:len(benInt)-1], 10, 64)
 }
 
-//extracts the data from a bencoded int and returns as a bool
 func (r benReader) readBenBool() (bool, error) {
 	benInt, err := r.b.ReadString(byte('e'))
 	if err != nil {
@@ -203,8 +181,8 @@ func (r benReader) readBenList(v reflect.Value) error {
 	//this is the type that we will expect to be contained in the bencoded string.
 	e := reflect.New(v.Type().Elem()).Elem()
 	//loop and call decode for each bencoded element that you traverse
-	//until you traverse an 'e' who is acting as the first byte of a bencoded value.
-	//after decoding, copy the decoded value to the slice (v).
+	//until you traverse an 'e' who is the first byte of a bencoded value.
+	//after decoding, append the decoded value to the slice (v).
 	for {
 		if b, err = r.b.ReadByte(); err != nil {
 			return err
@@ -222,10 +200,11 @@ func (r benReader) readBenList(v reflect.Value) error {
 		}
 		reflect.Append(v, e)
 	}
+	return nil
 }
 
-//v is either a reflect.Map or a reflect.Struct.
-func (r benReader) readBenDict(v reflect.Value) error {
+//v can be only of type reflect.Map.
+func (r benReader) readBenDictMap(v reflect.Value) error {
 	b, err := r.b.ReadByte()
 	if err != nil {
 		return err
@@ -233,56 +212,120 @@ func (r benReader) readBenDict(v reflect.Value) error {
 	if b != 'd' {
 		return errors.New("Bencoded has dict whereas data structure doesn't.")
 	}
-	//create a new value whos type is the type of the elements of v.
-	//this is the type that we will expect to be contained in the bencoded string.
-	//e := reflect.New(v.Type().Elem()).Elem()
 	t := v.Type()
-	switch t.Kind() {
-	case reflect.Struct:
-
-		for i := 0; i < v.NumField(); i++ {
-			fv := v.Field(i)
-			sf := fv.Type().Field()
-			err := decode(r, fv)
-
+	keyType := t.Key()
+	elemType := t.Elem()
+	if keyType.Kind() != reflect.String {
+		return errors.New("Maps should have keys of type string")
+	}
+	//create two zeroed values - the first represents the
+	//map's key type and the second the map's value's type.
+	//These are the types that we expect the bencoded string to have.
+	keyVal := reflect.New(keyType).Elem()
+	elemVal := reflect.New(elemType).Elem()
+	//Iterate and start decoding values until you see an 'e' as the
+	//first byte of a bencoded value.
+	for {
+		if b, err = r.b.ReadByte(); err != nil {
+			return err
 		}
-		b, err = r.b.ReadByte()
+		if b == 'e' {
+			break
+		}
+		err = r.b.UnreadByte()
 		if err != nil {
 			return err
 		}
-		if b != 'e' {
-			return errors.New("No 'e' at end of dictionary")
+		err = decode(r, keyVal)
+		if err != nil {
+			return err
 		}
-	case reflect.Map:
-		keyType := t.Key()
-		elemType := t.Elem()
-		if keyType.Kind() != reflect.String { //or reflect.Slice containing uint8
-			return errors.New("Maps should have keys of type string")
+		err = decode(r, elemVal)
+		if err != nil {
+			return err
 		}
-		keyVal := reflect.New(keyType).Elem()
-		elemVal := reflect.New(elemType).Elem()
-		for {
-			if b, err = r.b.ReadByte(); err != nil {
-				return err
-			}
-			if b == 'e' {
-				break
-			}
-			err = r.b.UnreadByte()
-			if err != nil {
-				return err
-			}
-			err = decode(r, keyVal)
-			if err != nil {
-				return err
-			}
-			err = decode(r, elemVal)
-			if err != nil {
-				return err
-			}
-			v.SetMapIndex(keyVal, elemVal)
-		}
-	default:
-		return errors.New("Expecting struct or map in readBenDict")
+		v.SetMapIndex(keyVal, elemVal)
 	}
+	return nil
+}
+
+//v can be only of type reflect.Struct.
+func (r benReader) readBenDictStruct(v reflect.Value) error {
+	b, err := r.b.ReadByte()
+	if err != nil {
+		return err
+	}
+	if b != 'd' {
+		return errors.New("Bencoded has dict whereas data structure doesn't.")
+	}
+	for i := 0; i < v.NumField(); i++ {
+		//ignore fields that have bencode tag set to '-'.
+		if val := v.Type().Field(i).Tag.Get("bencode"); val == "-" {
+			continue
+		}
+		//we may want to use the bencoded key to compare it with
+		//the struct field name, but ignore it for now.
+		_, err := r.readBenString()
+		if err != nil {
+			return err
+		}
+		fv := v.Field(i)
+		err = decode(r, fv)
+		if err != nil {
+			return err
+		}
+	}
+	b, err = r.b.ReadByte()
+	if err != nil {
+		return err
+	}
+	if b != 'e' {
+		return errors.New("No 'e' at end of dictionary")
+	}
+	return nil
+}
+
+//if we have a nil interface, then we dont
+//know which bencoded type to expect. What
+//we can do is set the interface to the type
+//the next bencoded values will be.
+func handleNilInterface(r benReader, v reflect.Value) error {
+	b, err := r.b.ReadByte()
+	if err != nil {
+		return err
+	}
+	switch {
+	case b == 'i':
+		var num int64
+		err = setNilInterface(r, v, reflect.ValueOf(num))
+	case b >= 48 && b <= 57:
+		var s string
+		err = setNilInterface(r, v, reflect.ValueOf(s))
+	case b == 'l':
+		err = setNilInterface(r, v, reflect.ValueOf([]interface{}{}))
+	case b == 'd':
+		err = setNilInterface(r, v, reflect.ValueOf(map[string]interface{}{}))
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//set the specified value (v) to interface (i), and call decode
+//on the no longer nil interface.Before decode, call unreadbyte
+//to unread the byte read from handleNilInterface. The goal of
+//this func is to fill a nil-empty interface with an underlying
+//type in a transparent way.
+func setNilInterface(r benReader, i, v reflect.Value) error {
+	i.Set(v)
+	err := r.b.UnreadByte()
+	if err != nil {
+		return err
+	}
+	err = decode(r, i)
+	if err != nil {
+		return err
+	}
+	return nil
 }
