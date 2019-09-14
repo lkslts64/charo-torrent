b55d94909342f655a6261fbb9aa76fac54b002c5
Loukas Litsos <lkslts64@gmail.com>
2019-09-14 20:29:34 +0300

    omitempty struct: a field is ommited if has zero value

--- bencode/encode.go
+++ bencode/encode.go
@@ -124,7 +124,8 @@ func encode(v reflect.Value, b *bytes.Buffer) error {
 		for i := 0; i < v.NumField(); i++ {
 			fvalue := v.FieldByName(sf[i].Name)
 			//if bencode tag == "-" OR field is a nil pointer and struct tag == omitempty , then ignore this field.
-			if sf[i].Tag.Get("bencode") == "-" || (fvalue.Type().Kind() == reflect.Ptr && !fvalue.Elem().IsValid() && sf[i].Tag.Get("empty") == "omit") {
+			//TODO:fix this. we will omit fields with empty:omit if they have the zero value for their type. (e.g if an integer i == 0 then we omit.)
+			if sf[i].Tag.Get("bencode") == "-" || (sf[i].Tag.Get("empty") == "omit" && isZero(v)) {
 				continue
 			}
 			//encode string and field
@@ -158,6 +159,31 @@ func handleNilPtr(t reflect.Type, b *bytes.Buffer) {
 		b.WriteString("de")
 
 	}
+}
+
+//isZero checks if v is a zero valued type
+//e.g if i is integer and i has value 0 then
+//isZero returns true.
+func isZero(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Func, reflect.Map, reflect.Slice:
+		return v.IsNil()
+	case reflect.Array:
+		z := true
+		for i := 0; i < v.Len(); i++ {
+			z = z && isZero(v.Index(i))
+		}
+		return z
+	case reflect.Struct:
+		z := true
+		for i := 0; i < v.NumField(); i++ {
+			z = z && isZero(v.Field(i))
+		}
+		return z
+	}
+	// Compare other types directly:
+	z := reflect.Zero(v.Type())
+	return v.Interface() == z.Interface()
 }
 
 type string_reflect []reflect.Value
