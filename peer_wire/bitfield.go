694029d40d5373ba790ab867b5b8070af4ce0bb3
Loukas Litsos <lkslts64@gmail.com>
2019-12-13 14:50:13 +0200

    Move bitfield to wire pkg & Test

--- peer_wire/bitfield.go
+++ peer_wire/bitfield.go
@@ -1,14 +1,15 @@
-package torrent
+package peer_wire
 
 import "math"
 
+//BitField is zero based index
 type BitField []byte
 
-func NewBitField(numPieces int) BitField {
+func newBitField(numPieces int) BitField {
 	return make([]byte, int(math.Ceil(float64(numPieces)/8.0)))
 }
 
-func (bf BitField) hasPiece(i uint32) bool {
+func (bf BitField) HasPiece(i uint32) bool {
 	index := i / 8
 	mask := byte(1 << (7 - i%8))
 	if bf[index]&mask > 0 {
@@ -17,7 +18,7 @@ func (bf BitField) hasPiece(i uint32) bool {
 	return false
 }
 
-func (bf BitField) setPiece(i uint32) {
+func (bf BitField) SetPiece(i uint32) {
 	index := i / 8
 	mask := byte(1 << (7 - i%8))
 	bf[index] |= mask
---
+++ peer_wire/bitfield_test.go
@@ -0,0 +1,31 @@
+package peer_wire
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBitfield(t *testing.T) {
+	bf := newBitField(16)
+	assert.Equal(t, 2, len(bf))
+	bf = newBitField(15)
+	assert.Equal(t, 2, len(bf))
+	bf = newBitField(17)
+	assert.Equal(t, 3, len(bf))
+	bf.SetPiece(10)
+	assert.Equal(t, byte(0x20), bf[1])
+	assert.Equal(t, true, bf.HasPiece(10))
+	bf.SetPiece(17)
+	assert.Equal(t, byte(0x40), bf[2])
+	bf.SetPiece(16)
+	assert.Equal(t, byte(0xc0), bf[2])
+	for i := 0; i < 17; i++ {
+		switch i {
+		case 10, 17, 16:
+			assert.Equal(t, true, bf.HasPiece(uint32(i)))
+		default:
+			assert.Equal(t, false, bf.HasPiece(uint32(i)))
+		}
+	}
+}
