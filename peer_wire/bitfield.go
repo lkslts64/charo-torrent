6741bb1df0aec094bc5eaadc191d1391d905f7dc
Loukas Litsos <lkslts64@gmail.com>
2019-12-29 20:02:20 +0200

    Add bitfield methods

--- peer_wire/bitfield.go
+++ peer_wire/bitfield.go
@@ -7,28 +7,34 @@ import "math/bits"
 //BitField is zero based index
 type BitField []byte
 
+//how much length a BitField will have if created with numPieces as argument
+func BfLen(numPieces int) int {
+	return int(math.Ceil(float64(numPieces) / 8.0))
+}
+
 func NewBitField(numPieces int) BitField {
-	return make([]byte, int(math.Ceil(float64(numPieces)/8.0)))
+	return make([]byte, BfLen(numPieces))
 }
 
-func (bf BitField) HasPiece(i uint32) bool {
-	index := i / 8
-	mask := byte(1 << (7 - i%8))
-	if bf[index]&mask > 0 {
-		return true
-	}
-	return false
+func (bf BitField) Valid(expectedLen int) bool {
+	return len(bf) != BfLen(expectedLen)
 }
 
-func (bf BitField) SetPiece(i uint32) {
+func (bf BitField) HasPiece(i int) bool {
 	index := i / 8
-	mask := byte(1 << (7 - i%8))
+	mask := uint8(1 << uint(7-i%8))
+	return uint8(bf[index]&mask) > 0
+}
+
+func (bf BitField) SetPiece(i int) {
+	index := i / 8
+	mask := byte(1 << uint(7-i%8))
 	bf[index] |= mask
 }
 
 func (bf BitField) FilterNotSet() (set []int) {
 	for i := 0; i < len(bf)*8; i++ {
-		if bf.HasPiece(uint32(i)) {
+		if bf.HasPiece(i) {
 			set = append(set, i)
 		}
 	}
