f979f33b05598f114d9ce9b1bdfd09ec33205909
Loukas Litsos <lkslts64@gmail.com>
2020-01-01 22:10:38 +0200

    Add conn state

---
+++ torrent/conn_state.go
@@ -0,0 +1,23 @@
+package torrent
+
+type connState struct {
+	amInterested bool
+	amChoking    bool
+	isInterested bool
+	isChoking    bool
+}
+
+func newConnState() *connState {
+	return &connState{
+		amChoking: true,
+		isChoking: true,
+	}
+}
+
+func (cs *connState) canUpload() bool {
+	return !cs.amChoking && cs.isInterested
+}
+
+func (cs *connState) canDownload() bool {
+	return !cs.isChoking && cs.amInterested
+}
