b57e67e13c8d7050025bed8824b5c5e876e721f5
Loukas Litsos <lkslts64@gmail.com>
2020-01-22 04:20:17 +0200

    Eliminate deadlocks possibility & Refactor
    Now, if we want to send an event to eventCh but its full. we block
    but we are also 'selecting' commandCh (see sendPeerEvent) to
    check if we have any incoming commands. This prevents the
    situation of blocking at eventCh and at commandCh for ever.

--- torrent/conn.go
+++ torrent/conn.go
@@ -16,14 +16,10 @@ import (
 
 const (
 	//the size could be t.reqq?
-	readChSize = 250
-	//jobChSize   = maxConns * eventChSize * 10
-	eventChSize = 250
+	readChSize    = 250
+	eventChSize   = 20
+	commandChSize = eventChSize
 )
-
-//jobs that we 'll get are proportional to events a conn emits.
-//this is upper bound on jobs to be produced - we can decrease it more
-var jobChSize = maxConns*eventChSize*(maxOnFlight+10) + 10
 
 const (
 	keepAliveInterval time.Duration = 2 * time.Minute
@@ -44,11 +40,13 @@ type conn struct {
 	//the two goroutines
 	state connState
 	//chanel that we receive msgs to be sent (generally)
-	//jobCh chan job - infinite chan
-	//we want to avoid deadlocks between jobCh and eventCh.
+	//commandCh chan job - infinite chan
+	//we want to avoid deadlocks between commandCh and eventCh.
 	//Also we dont want master to block until workers(peerConns)
 	//receive jobs (they are doing heavy I/O).
-	jobCh chan interface{}
+	commandCh chan interface{}
+	//chan to signal that conn should be dropped
+	drop chan struct{}
 	//channel that we send received msgs
 	eventCh        chan interface{}
 	keepAliveTimer *time.Timer
@@ -63,7 +61,6 @@ type conn struct {
 	//how many blocks we are waiting to be forwarded to us
 	waitingBlocks int
 	//TODO: we 'll need this later.
-	//drop        chan struct{}
 	peerBf bitmap.Bitmap
 	myBf   bitmap.Bitmap
 	peerID []byte
@@ -75,30 +72,32 @@ func newConn(t *Torrent, cn net.Conn, peerID []byte) *conn {
 		cl: t.cl,
 		t:  t,
 		//TODO:change logger output to a file
-		logger:  log.New(os.Stdout, string(peerID), log.LstdFlags),
-		cn:      cn,
-		state:   newConnState(),
-		jobCh:   make(chan interface{}, jobChSize),
-		eventCh: make(chan interface{}, eventChSize),
-		rq:      newRequestQueuer(),
-		//drop:        make(chan struct{}),
-		//peerReqq: peerReqq,
-		peerReqs: make(map[block]struct{}),
+		logger:    log.New(os.Stdout, string(peerID), log.LstdFlags),
+		cn:        cn,
+		state:     newConnState(),
+		commandCh: make(chan interface{}, commandChSize),
+		eventCh:   make(chan interface{}, eventChSize),
+		drop:      make(chan struct{}),
+		rq:        newRequestQueuer(),
+		peerReqs:  make(map[block]struct{}),
 	}
 	//inform others about the new conn
 	t.newConnCh <- &connChansInfo{
-		c.jobCh,
+		c.commandCh,
 		c.eventCh,
+		c.drop,
 	}
 	return c
 }
 
-func (pc *conn) close() {
-	pc.cn.Close()
+func (c *conn) close() {
+	c.cn.Close()
 	//send event that we closed and after close chan - avoid leak goroutines
-	pc.eventCh <- *new(connDroped)
-	close(pc.eventCh)
-	pc.logger.Println("closed connection")
+	//TODO:handle this case, we dont want to receive from cmdCh we just want to exit
+	//maybe a new connDrop channel or SEND a conn droped in aggregateEvents in Torrent?
+	c.sendPeerEvent(connDroped{})
+	close(c.eventCh)
+	c.logger.Println("closed connection")
 }
 
 type readResult struct {
@@ -106,63 +105,59 @@ type readResult struct {
 	err error
 }
 
-func (pc *conn) mainLoop() error {
+func (c *conn) mainLoop() error {
 	var err error
 	//pc.init()
 	readCh := make(chan *peer_wire.Msg, readChSize)
 	readErrCh := make(chan error, 1)
 	//we need o quit chan in order to not leak readMsgs goroutine
 	quit, idle := make(chan struct{}), make(chan struct{})
-	defer pc.close()
+	defer c.close()
 	defer close(quit)
-	go pc.readMsgs(readCh, idle, quit, readErrCh)
+	go c.readMsgs(readCh, idle, quit, readErrCh)
 	nilOnEOF := func(err error) error {
 		if errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) {
 			return nil
 		}
 		return err
 	}
-	pc.keepAliveTimer = time.NewTimer(keepAliveSendFreq)
+	c.keepAliveTimer = time.NewTimer(keepAliveSendFreq)
 	for {
-		//prioritize jobs
 		select {
-		case act := <-pc.jobCh:
-			//TODO:pass values to job ch as interface{}
-			//no need for job type wrapper
-			err = pc.parseJob(act.(job))
-		default:
-			select {
-			case act := <-pc.jobCh:
-				//TODO:pass values to job ch as interface{}
-				//no need for job type wrapper
-				err = pc.parseJob(act.(job))
-			case <-pc.t.done:
-				return nil
-			case msg := <-readCh:
-				err = pc.parsePeerMsg(msg)
-			case err = <-readErrCh:
-			case <-idle:
-				err = errors.New("peer idle")
-			case <-pc.keepAliveTimer.C:
-				err = pc.sendKeepAlive()
-			}
+		case cmd := <-c.commandCh:
+			err = c.parseCommand(cmd)
+		case msg := <-readCh:
+			err = c.parsePeerMsg(msg)
+		case err = <-readErrCh:
+		case <-c.t.done:
+			return nil
+		case <-c.drop:
+			return nil
+		case <-idle:
+			err = errors.New("peer idle")
+			return nil
+		case <-c.keepAliveTimer.C:
+			err = c.sendKeepAlive()
 		}
 		if err != nil {
 			switch err = nilOnEOF(err); err {
 			case nil:
-				pc.logger.Println("lost connection with peer")
+				c.logger.Println("lost connection with peer")
 			default:
-				pc.logger.Println(err)
+				c.logger.Println(err)
 			}
 			return err
 		}
-		if pc.wantBlocks() {
-			pc.eventCh <- *new(wantBlocks)
-			pc.waitingBlocks = maxOnFlight
-			pc.logger.Println("want blocks send")
+		if c.wantBlocks() {
+			err = c.sendPeerEvent(wantBlocks{})
+			if err != nil {
+				return err
+			}
+			c.waitingBlocks = maxOnFlight
+			c.logger.Println("want blocks send")
 		}
-		if pc.notUseful() {
-			pc.logger.Println("conn is not useful anymore for both ends")
+		if c.notUseful() {
+			c.logger.Println("conn is not useful anymore for both ends")
 			return nil
 		}
 	}
@@ -170,7 +165,7 @@ func (pc *conn) mainLoop() error {
 
 //read msgs from remote peer
 //run on seperate goroutine
-func (pc *conn) readMsgs(readCh chan<- *peer_wire.Msg, idle, quit chan struct{},
+func (c *conn) readMsgs(readCh chan<- *peer_wire.Msg, idle, quit chan struct{},
 	errCh chan error) {
 	var msg *peer_wire.Msg
 	var err error
@@ -180,7 +175,7 @@ loop:
 		//we won't block on read forever cause conn will close
 		//if main goroutine exits
 		go func() {
-			msg, err = peer_wire.Read(pc.cn)
+			msg, err = peer_wire.Read(c.cn)
 			readDone <- readResult{msg, err}
 		}()
 		//break from loops in not required
@@ -196,29 +191,29 @@ loop:
 			switch res.msg.Kind {
 			case peer_wire.Request:
 				b := reqMsgToBlock(msg)
-				pc.muPeerReqs.Lock()
+				c.muPeerReqs.Lock()
 				//avoid flooding readCh by not sending requests that are
 				//already requested from peer and by discarding requests when
 				//our buffer is full.
-				switch _, ok := pc.peerReqs[b]; {
+				switch _, ok := c.peerReqs[b]; {
 				case ok:
-					pc.logger.Printf("peer send duplicate request for block %v\n", b)
-				case len(pc.peerReqs) >= pc.t.reqq:
-					pc.logger.Print("peer requests buffer is full\n")
+					c.logger.Printf("peer send duplicate request for block %v\n", b)
+				case len(c.peerReqs) >= c.t.reqq:
+					c.logger.Print("peer requests buffer is full\n")
 				default: //all good
-					pc.peerReqs[b] = struct{}{}
+					c.peerReqs[b] = struct{}{}
 					sendToChan = true
 				}
-				pc.muPeerReqs.Unlock()
+				c.muPeerReqs.Unlock()
 			case peer_wire.Cancel:
 				b := reqMsgToBlock(msg)
-				pc.muPeerReqs.Lock()
-				if _, ok := pc.peerReqs[b]; ok {
-					delete(pc.peerReqs, b)
+				c.muPeerReqs.Lock()
+				if _, ok := c.peerReqs[b]; ok {
+					delete(c.peerReqs, b)
 				} else {
 					latecomerCancels.Inc()
 				}
-				pc.muPeerReqs.Unlock()
+				c.muPeerReqs.Unlock()
 			default:
 				sendToChan = true
 			}
@@ -251,139 +246,175 @@ func (c *conn) peerSeeding() bool {
 	return c.peerBf.Len() == c.t.numPieces()
 }
 
-func (pc *conn) sendKeepAlive() error {
+func (c *conn) sendKeepAlive() error {
 	err := (&peer_wire.Msg{
 		Kind: peer_wire.KeepAlive,
-	}).Write(pc.cn)
+	}).Write(c.cn)
 	if err != nil {
 		return err
 	}
-	pc.keepAliveTimer.Reset(keepAliveSendFreq)
+	c.keepAliveTimer.Reset(keepAliveSendFreq)
 	return nil
 }
 
-func (pc *conn) parseJob(j job) (err error) {
-	switch v := j.val.(type) {
+func (c *conn) parseCommand(cmd interface{}) (err error) {
+	switch v := cmd.(type) {
 	case *peer_wire.Msg:
 		switch v.Kind {
 		case peer_wire.Interested:
-			pc.state.amInterested = true
+			c.state.amInterested = true
 		case peer_wire.NotInterested:
-			pc.state.amInterested = false
+			c.state.amInterested = false
 		case peer_wire.Choke:
-			pc.state.amChoking = true
+			c.state.amChoking = true
 		case peer_wire.Unchoke:
-			pc.state.amChoking = false
+			c.state.amChoking = false
 		case peer_wire.Have:
-			pc.myBf.Set(int(v.Index), true)
+			c.myBf.Set(int(v.Index), true)
 			//Surpress have msgs.
 			//From https://wiki.theory.org/index.php/BitTorrentSpecification:
 			//At the same time, it may be worthwhile to send a HAVE message
 			//to a peer that has that piece already since it will be useful in
 			//determining which piece is rare.
-			if pc.peerBf.Get(int(v.Index)) {
+			//TODO:change to:
+			// if pc.peerBf.Get(int(v.Index)) && !pc.peerBf.peerSeeding() {
+			if c.peerBf.Get(int(v.Index)) {
 				return
 			}
 		}
-		err = pc.sendMsg(v)
+		err = c.sendMsg(v)
 	case []block:
-		pc.waitingBlocks -= len(v)
+		c.waitingBlocks -= len(v)
 		var ok, ready bool
 		for _, bl := range v {
-			if ok, ready = pc.rq.queue(bl); !ok {
+			if ok, ready = c.rq.queue(bl); !ok {
 				panic("received blocks but can't queue them")
 			}
 			if ready {
-				pc.sendMsg(bl.reqMsg())
+				c.sendMsg(bl.reqMsg())
 			}
 		}
 	case bitmap.Bitmap: //this equivalent to bitfield, we encode and write to wire.
-		pc.myBf = v
-		pc.sendMsg(&peer_wire.Msg{
+		c.myBf = v
+		c.sendMsg(&peer_wire.Msg{
 			Kind: peer_wire.Bitfield,
-			Bf:   pc.encodeBitMap(pc.myBf),
+			Bf:   c.encodeBitMap(c.myBf),
 		})
 	case verifyPiece:
-		correct := pc.t.storage.HashPiece(int(v), pc.t.PieceLen(uint32(v)))
-		pc.eventCh <- pieceVerificationOk{
+		correct := c.t.storage.HashPiece(int(v), c.t.pieceLen(uint32(v)))
+		err = c.sendPeerEvent(pieceHashed{
 			pieceIndex: int(v),
 			ok:         correct,
-		}
+		})
 	case haveInfo:
-		pc.haveInfo = true
+		c.haveInfo = true
 	case seeding:
-		pc.amSeeding = true
+		c.amSeeding = true
 	}
 	return
 }
 
-func (pc *conn) sendMsg(msg *peer_wire.Msg) error {
+func (c *conn) sendMsg(msg *peer_wire.Msg) error {
 	//is mandatory to stop timer and recv from chan
 	//in order to reset it
-	if !pc.keepAliveTimer.Stop() {
-		<-pc.keepAliveTimer.C
+	if !c.keepAliveTimer.Stop() {
+		<-c.keepAliveTimer.C
 		err := (&peer_wire.Msg{
 			Kind: peer_wire.KeepAlive,
-		}).Write(pc.cn)
+		}).Write(c.cn)
 		if err != nil {
 			return err
 		}
 	}
-	pc.keepAliveTimer.Reset(keepAliveSendFreq)
-	err := msg.Write(pc.cn)
+	c.keepAliveTimer.Reset(keepAliveSendFreq)
+	err := msg.Write(c.cn)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (pc *conn) parsePeerMsg(msg *peer_wire.Msg) (err error) {
-	stateChange := func(currState, futureState bool) bool {
+func (c *conn) parsePeerMsg(msg *peer_wire.Msg) (err error) {
+	stateChange := func(currState, futureState bool) (bool, error) {
 		if currState == futureState {
-			return futureState
+			return futureState, nil
 		}
-		pc.eventCh <- msg
-		return futureState
+		return futureState, c.sendPeerEvent(msg)
 	}
 	switch msg.Kind {
 	case peer_wire.KeepAlive, peer_wire.Port:
 	case peer_wire.Interested:
-		pc.state.isInterested = stateChange(pc.state.isInterested, true)
+		c.state.isInterested, err = stateChange(c.state.isInterested, true)
 	case peer_wire.NotInterested:
-		pc.state.isInterested = stateChange(pc.state.isInterested, false)
+		c.state.isInterested, err = stateChange(c.state.isInterested, false)
 	case peer_wire.Choke:
-		pc.state.isChoking = stateChange(pc.state.isChoking, true)
-		pc.discardBlocks()
+		c.state.isChoking, err = stateChange(c.state.isChoking, true)
+		err = c.discardBlocks()
 	case peer_wire.Unchoke:
-		pc.state.isChoking = stateChange(pc.state.isChoking, false)
+		c.state.isChoking, err = stateChange(c.state.isChoking, false)
 	case peer_wire.Piece:
-		err = pc.onPieceMsg(msg)
+		err = c.onPieceMsg(msg)
 	case peer_wire.Request:
-		err = pc.upload(msg)
+		err = c.upload(msg)
 	case peer_wire.Have:
-		if pc.peerBf.Get(int(msg.Index)) {
-			pc.logger.Printf("peer send duplicate Have msg of piece %d\n", msg.Index)
+		if c.peerBf.Get(int(msg.Index)) {
+			c.logger.Printf("peer send duplicate Have msg of piece %d\n", msg.Index)
 			return
 		}
-		pc.peerBf.Set(int(msg.Index), true)
-		pc.eventCh <- msg
+		c.peerBf.Set(int(msg.Index), true)
+		err = c.sendPeerEvent(msg)
 	case peer_wire.Bitfield:
-		if !pc.peerBf.IsEmpty() {
+		if !c.peerBf.IsEmpty() {
 			err = errors.New("peer: send bitfield twice or have before bitfield")
+			return
 		}
 		var tmp *bitmap.Bitmap
-		tmp, err = pc.decodeBitfield(msg.Bf)
-		if err != nil {
+		if tmp, err = c.decodeBitfield(msg.Bf); err != nil {
 			return
 		}
-		pc.peerBf = *tmp
-		pc.eventCh <- pc.peerBf.Copy()
+		c.peerBf = *tmp
+		err = c.sendPeerEvent(c.peerBf.Copy())
 	case peer_wire.Extended:
-		pc.onExtended(msg)
+		c.onExtended(msg)
 	default:
 		err = errors.New("unknown msg kind received")
 	}
 	return
+}
+
+func (c *conn) sendPeerEvent(e interface{}) error {
+	var err error
+	for {
+		//we dont just send e to eventCh because if the former chan is full,
+		//then commandCh may also become full and we will deadlock.
+		select {
+		case c.eventCh <- e:
+			return nil
+		default:
+			//receive command while waiting for event to be send, we dont want
+			//to deadlock.
+			select {
+			case cmd := <-c.commandCh:
+				err = c.parseCommand(cmd)
+			case c.eventCh <- e:
+				return nil
+			//pretend we lost connection
+			case <-c.t.done:
+				err = io.EOF
+			case <-c.drop:
+				err = io.EOF
+			//TODO:should we have idle chan inside struct conn?
+			/*case <-idle:
+			err = errors.New("peer idle")
+			return nil*/
+			case <-c.keepAliveTimer.C:
+				err = c.sendKeepAlive()
+			}
+		}
+		if err != nil {
+			return err
+		}
+	}
 }
 
 func (c *conn) encodeBitMap(bm bitmap.Bitmap) (bf peer_wire.BitField) {
@@ -433,13 +464,15 @@ func (pc *conn) peerBitFieldValid() bool {
 
 type metainfoSize int64
 
-func (pc *conn) onExtended(msg *peer_wire.Msg) (err error) {
+func (c *conn) onExtended(msg *peer_wire.Msg) (err error) {
 	switch v := msg.ExtendedMsg.(type) {
 	case peer_wire.ExtHandshakeDict:
-		pc.exts, err = v.Extensions()
-		if _, ok := pc.exts[peer_wire.ExtMetadataName]; ok {
+		c.exts, err = v.Extensions()
+		if _, ok := c.exts[peer_wire.ExtMetadataName]; ok {
 			if msize, ok := v.MetadataSize(); ok {
-				pc.eventCh <- metainfoSize(msize)
+				if err = c.sendPeerEvent(metainfoSize(msize)); err != nil {
+					//error
+				}
 			}
 		}
 	case peer_wire.MetadataExtMsg:
@@ -447,8 +480,8 @@ func (pc *conn) onExtended(msg *peer_wire.Msg) (err error) {
 		case peer_wire.MetadataDataID:
 		case peer_wire.MetadataRejID:
 		case peer_wire.MetadataReqID:
-			if !pc.haveInfo {
-				pc.sendMsg(&peer_wire.Msg{
+			if !c.haveInfo {
+				c.sendMsg(&peer_wire.Msg{
 					Kind:       peer_wire.Extended,
 					ExtendedID: peer_wire.ExtMetadataID,
 					ExtendedMsg: peer_wire.MetadataExtMsg{
@@ -466,115 +499,114 @@ func (pc *conn) onExtended(msg *peer_wire.Msg) (err error) {
 	return nil
 }
 
-func (pc *conn) discardBlocks() {
-	if !pc.rq.empty() {
-		unsatisfiedRequests := pc.rq.discardAll()
-		pc.eventCh <- unsatisfiedRequests
+func (c *conn) discardBlocks() (err error) {
+	if !c.rq.empty() {
+		unsatisfiedRequests := c.rq.discardAll()
+		err = c.sendPeerEvent(discardedRequests(unsatisfiedRequests))
 	}
+	return
 }
 
 //TODO:Store bad peer so we wont accept them again if they try to reconnect
-func (pc *conn) upload(msg *peer_wire.Msg) error {
+func (c *conn) upload(msg *peer_wire.Msg) error {
 	bl := reqMsgToBlock(msg)
-	if pc.isCanceled(bl) {
+	if c.isCanceled(bl) {
 		return nil
 	}
 	//ensure we delete the request at every case
 	defer func() {
-		pc.muPeerReqs.Lock()
-		defer pc.muPeerReqs.Unlock()
-		delete(pc.peerReqs, bl)
+		c.muPeerReqs.Lock()
+		defer c.muPeerReqs.Unlock()
+		delete(c.peerReqs, bl)
 	}()
-	if !pc.haveInfo {
+	if !c.haveInfo {
 		return errors.New("peer send requested and we dont have info dict")
 	}
-	if !pc.state.canUpload() {
+	if !c.state.canUpload() {
 		//maybe we have choked the peer, but he hasnt been informed and
 		//thats why he send us request, dont drop conn just ignore the req
-		pc.logger.Print("peer send request msg while choked\n")
+		c.logger.Print("peer send request msg while choked\n")
 		return nil
 	}
 	if bl.len > maxRequestBlockSz {
 		return fmt.Errorf("request length out of range: %d", msg.Len)
 	}
 	//check that we have the requested piece
-	if !pc.myBf.Get(bl.pc) {
+	if !c.myBf.Get(bl.pc) {
 		return fmt.Errorf("peer requested piece we do not have")
 	}
 	//ensure the we dont exceed the end of the piece
-	if endOff := bl.off + bl.len; endOff > pc.t.PieceLen(uint32(bl.pc)) {
+	if endOff := bl.off + bl.len; endOff > c.t.pieceLen(uint32(bl.pc)) {
 		return fmt.Errorf("peer request exceeded length of piece: %d", endOff)
 	}
 	//read from db
 	//and write to conn
 	data := make([]byte, bl.len)
-	if err := pc.t.readBlock(data, bl.pc, bl.off); err != nil {
+	if err := c.t.readBlock(data, bl.pc, bl.off); err != nil {
 		return nil
 	}
-	pc.sendMsg(&peer_wire.Msg{
+	c.sendMsg(&peer_wire.Msg{
 		Kind:  peer_wire.Piece,
 		Index: msg.Index,
 		Begin: msg.Begin,
 		Block: data,
 	})
-	pc.eventCh <- uploadedBlock(bl)
-	return nil
+	return c.sendPeerEvent(uploadedBlock(bl))
 }
 
-func (pc *conn) isCanceled(b block) bool {
-	pc.muPeerReqs.Lock()
-	defer pc.muPeerReqs.Unlock()
-	_, ok := pc.peerReqs[b]
+func (c *conn) isCanceled(b block) bool {
+	c.muPeerReqs.Lock()
+	defer c.muPeerReqs.Unlock()
+	_, ok := c.peerReqs[b]
 	return !ok
 }
 
 //store block and send another request if request queuer permits it.
-func (pc *conn) onPieceMsg(msg *peer_wire.Msg) error {
+func (c *conn) onPieceMsg(msg *peer_wire.Msg) error {
 	var ready block
 	var ok bool
 	bl := reqMsgToBlock(msg.Request())
-	if ready, ok = pc.rq.deleteCompleted(bl); !ok {
+	if ready, ok = c.rq.deleteCompleted(bl); !ok {
 		//We didnt' request this piece, but maybe it is useful
-		pc.logger.Printf("received unexpected block %v\n", bl)
-		if !pc.haveInfo {
+		c.logger.Printf("received unexpected block %v\n", bl)
+		if !c.haveInfo {
 			return errors.New("peer send piece while we dont have info")
 		}
-		if !pc.t.pieceValid(bl.pc) {
+		if !c.t.pieceValid(bl.pc) {
 			return errors.New("peer send piece doesn't exist")
 		}
 		//TODO: check if we have the particular block
-		if pc.myBf.Get(bl.pc) {
-			pc.logger.Printf("peer send block of piece we already have: %v\n", bl)
+		if c.myBf.Get(bl.pc) {
+			c.logger.Printf("peer send block of piece we already have: %v\n", bl)
 			return nil
 		}
 		var length int
 		var err error
-		if length, err = pc.t.pieces.pcs[bl.pc].blockLenSafe(bl.off); err != nil {
+		if length, err = c.t.pieces.pcs[bl.pc].blockLenSafe(bl.off); err != nil {
 			switch {
 			case errors.Is(err, errDivOffset):
 				//peer gave us a useful block but with offset % blockSz != 0
 				//We dont punish him but we dont save to storage also, we dont
 				//want to mix our block offsets (we have precomputed which blocks
 				//to ask for).
-				pc.logger.Println(err)
+				c.logger.Println(err)
 				return nil
 			case errors.Is(err, errLargeOffset):
 				return err
 			}
 		}
 		if length != bl.len {
-			pc.logger.Println("length of piece msg is wrong")
+			c.logger.Println("length of piece msg is wrong")
 			return nil
 		}
 	}
-	if pc.state.canDownload() && (ready != block{}) {
-		pc.sendMsg(ready.reqMsg())
+	if c.state.canDownload() && (ready != block{}) {
+		c.sendMsg(ready.reqMsg())
 	}
-	if err := pc.t.writeBlock(msg.Block, bl.pc, bl.off); err != nil {
+	if err := c.t.writeBlock(msg.Block, bl.pc, bl.off); err != nil {
 		return nil
 	}
-	pc.eventCh <- downloadedBlock(bl)
-	return nil
+	return c.sendPeerEvent(downloadedBlock(bl))
 }
 
 //TODO: master hold piece struct and manages them.
