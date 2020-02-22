856b863be4b012c8d3a33f19f61094cf1ed56363
Loukas Litsos <lkslts64@gmail.com>
2020-02-22 20:17:17 +0200

    Add pprof options

--- cmd/charo-download/main.go
+++ cmd/charo-download/main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"runtime"
+	"runtime/pprof"
 	"time"
 
 	"github.com/gosuri/uilive"
 	"github.com/lkslts64/charo-torrent/torrent"
 )
 
+var torrentFile = flag.String("torrentfile", "", "read the contents of the torrent `file`")
+var cpuprofile = flag.String("cpuprof", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprof", "", "write memory profile to `file`")
+
 func main() {
-	args := os.Args[1:]
-	if len(args) != 1 {
-		log.Fatal("wrong # of args")
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
 	}
+	//
 	cfg, err := torrent.DefaultConfig()
 	if err != nil {
 		log.Fatal(err)
@@ -23,7 +39,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	t, err := cl.AddFromFile(args[0])
+	t, err := cl.AddFromFile(*torrentFile)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -42,4 +58,16 @@ loop:
 		}
 	}
 	fmt.Println("downloaded torrent")
+	//
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close()
+		runtime.GC() // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+	}
 }
