23e40bc6f61b978a46daf402f4a6c786110c220a
Loukas Litsos <lkslts64@gmail.com>
2020-03-05 03:11:28 +0200

    Add locker and xported methods

--- torrent/tlocker.go
+++ torrent/tlocker.go
@@ -1,17 +1,30 @@
 package torrent
 
+import "errors"
+
 //this is the locking mechanism when someone wants to call exported methods on a
-//Torrent.
+//Torrent. Locking is implementing with channels
 type torrentLocker struct {
-	ch chan chan struct{}
+	ch     chan chan interface{}
+	closed chan struct{}
+	state  *State
 }
 
-func (l *torrentLocker) lock() chan struct{} {
-	ch := make(chan struct{})
-	l.ch <- ch
-	return ch
+func (l *torrentLocker) lock() (chan interface{}, error) {
+	ch := make(chan interface{})
+	select {
+	//wait until mainLoop is notified that we want to take ownership of the data
+	case l.ch <- ch:
+		return ch, nil
+	case <-l.closed:
+		return nil, errors.New("torrent is closed")
+	}
 }
 
-func (l *torrentLocker) unlock(ch chan struct{}) {
-	ch <- struct{}{}
+func (l *torrentLocker) unlock(ch chan interface{}, v interface{}) {
+	if ch == nil {
+		panic("tlocker: unlock")
+	}
+	//notify mainLoop that we finished
+	ch <- v
 }
--- torrent/torrent_xported.go
+++ torrent/torrent_xported.go
@@ -1,57 +1,86 @@
 package torrent
 
 import (
+	"errors"
 	"io"
 	"strings"
 )
 
 //AddPeers establishes connections with peers.
-func (t *Torrent) AddPeers(peers ...Peer) {
-	switch t.state {
-	case Added:
-	case Closed:
-		return
-	case Running:
-		ch := t.locker.lock()
-		defer t.locker.unlock(ch)
-	default:
-		panic("unknown state")
+func (t *Torrent) AddPeers(peers ...Peer) error {
+	ch, err := t.locker.lock()
+	if err != nil {
+		return err
 	}
+	defer t.locker.unlock(ch, struct{}{})
 	t.gotPeers(peers)
+	return nil
 }
 
-func (t *Torrent) Download() <-chan struct{} {
-	t.state = Running
-	go t.mainLoop()
-	return t.downloadedAll
+//Swarm returns all known peers associated with this torrent.
+func (t *Torrent) Swarm() ([]Peer, error) {
+	ch, err := t.locker.lock()
+	if err != nil {
+		return nil, err
+	}
+	defer t.locker.unlock(ch, struct{}{})
+	return t.swarm(), nil
+}
+
+//Download downloads all the torrent's data. It requires the info first.
+func (t *Torrent) Download() error {
+	if err := t.download(); err != nil {
+		return err
+	}
+	select {
+	case <-t.downloadedData:
+		return nil
+	case <-t.closed:
+		return errors.New("torrent closed")
+	}
+}
+
+func (t *Torrent) download() error {
+	ch, err := t.locker.lock()
+	if err != nil {
+		return err
+	}
+	if t.downloadRequest {
+		return errors.New("already downloading")
+	}
+	t.downloadRequest = true
+	defer t.locker.unlock(ch, struct{}{})
+	return nil
 }
 
 //WriteStatus writes to w a human readable message about the status of the Torrent.
-func (t *Torrent) WriteStatus(w io.Writer) {
-	switch t.state {
-	case Added:
-	case Closed:
-		return
-	case Running:
-		ch := t.locker.lock()
-		defer t.locker.unlock(ch)
-	default:
-		panic("unknown state")
+func (t *Torrent) WriteStatus(w io.Writer) error {
+	ch, err := t.locker.lock()
+	if err != nil {
+		return err
 	}
+	defer t.locker.unlock(ch, struct{}{})
 	b := new(strings.Builder)
 	t.writeStatus(b)
 	w.Write([]byte(b.String()))
+	return nil
 }
 
-//dificult to use this due to type safety
-func (t *Torrent) withLockContext(f func()) {
-	switch t.state {
-	case Added:
-	case Closed:
+//Close closes all connections with peers.When called, all methods on this torrent return errors.
+//Close is safe to be called multiple times on the same torrent.
+func (t *Torrent) Close() {
+	if err := t.closeWithLock(); err != nil {
 		return
-	case Running:
-		ch := t.locker.lock()
-		defer t.locker.unlock(ch)
 	}
-	f()
+	t.cl.dropTorrent(t.mi.Info.Hash)
+}
+
+func (t *Torrent) closeWithLock() error {
+	ch, err := t.locker.lock()
+	if err != nil {
+		return err
+	}
+	defer t.locker.unlock(ch, struct{}{})
+	t.close()
+	return nil
 }
