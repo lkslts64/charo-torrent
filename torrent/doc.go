fa859cae504d64ee2150f6939c5b7f55cedfcf6b
Loukas Litsos <lkslts64@gmail.com>
2020-03-07 22:11:03 +0200

    Add doc.go

---
+++ torrent/doc.go
@@ -0,0 +1,11 @@
+/*
+Package torrent implements the BitTorrent protocol.
+It is designed to be simple and easy to use. A common workflow is to create a Client,
+add a torrent and then download it.
+
+	cl, _ := torrent.NewClient(nil)
+	t, _ := cl.AddFromFile("example.torrent")
+	t.Download()
+	fmt.Println("torrent downloaded!")
+*/
+package torrent
